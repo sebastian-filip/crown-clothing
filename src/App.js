12366ff3ee2e6d802cedb67362d565df3921089f
filipsebastian <filip.seba@gmail.com>
2020-02-27 16:24:05 +0200

    - adding the option to login with email and password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignOutPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 
 class App extends React.Component {
@@ -21,9 +21,22 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user})
-            //console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth){
+                const userRef = createUserProfileDocument(userAuth);
+
+                (await userRef).onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser : {
+                            id : snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+                });
+            }
+            this.setState({
+                currentUser : userAuth
+            })
         });
     }
 
